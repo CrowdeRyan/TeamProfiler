579ccc2bb90c3f49f1a1e79f3b493a0e5dd8d2cc
Ryan Crowder <crowderyan@gmail.com>
2021-07-07 20:31:44 -0400

    added team combiner (is that a word?)

--- util/generateHtml.js
+++ util/generateHtml.js
@@ -76,4 +76,25 @@ const generateHtml = (employees) => {
                 </div>
             `;
   };
+  const team = [];
+  team.push(
+    employees
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => createManager(manager))
+      .join("")
+  );
+  team.push(
+    employees
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => createEngineer(engineer))
+      .join("")
+  );
+  team.push(
+    employees
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => createIntern(intern))
+      .join("")
+  );
+
+  return team.join("");
 };
