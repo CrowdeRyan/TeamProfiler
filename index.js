1b7454786cba7a7cf0a779a535475190d2c9683a
Ryan Crowder <crowderyan@gmail.com>
2021-07-07 20:15:26 -0400

    added engineer prompts

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const employees = [];
 
 const questions = {
   // MANAGER QUESTIONS
-
   manager: [
     {
       type: "input",
@@ -59,6 +58,61 @@ const questions = {
         return valid || "Please enter an office number.";
       },
       filter: Number,
+    },
+  ],
+
+  // ENGINEER QUESTIONS
+  engineer: [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of the employee? \n",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("You must enter a name.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "number",
+      name: "id",
+      message: "What is the employee's ID number? \n",
+      validate: (idInput) => {
+        if (idInput) {
+          return true;
+        } else {
+          console.log("You must enter a 4-digit ID number.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the employee's email address? \n",
+      validate: (emailInput) => {
+        if (emailInput.includes("@") && emailInput.includes(".com")) {
+          return true;
+        } else {
+          console.log("\n You must enter a valid email address.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "\n What is the Engineer's GitHub username?",
+      validate: function (value) {
+        if (value) {
+          return true;
+        } else {
+          console.log("Please provide the GitHub username for this employee.");
+        }
+      },
     },
   ],
 };
