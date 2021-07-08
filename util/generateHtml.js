985ee57c2895b0be4c56335d6c31545e32dcaa94
Ryan Crowder <crowderyan@gmail.com>
2021-07-07 20:32:48 -0400

    added export html template

--- util/generateHtml.js
+++ util/generateHtml.js
@@ -98,3 +98,30 @@ const generateHtml = (employees) => {
 
   return team.join("");
 };
+
+module.exports = (employeeHtml) => {
+  return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>My Team</title>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" integrity="sha512-UJfAaOlIRtdR+0P6C3KUoTDAxVTuy3lnSXLyLKlHYJlcSU8Juge/mjeaxDNMlw9LgeIotgz5FP8eUQPhX1q10A==" crossorigin="anonymous" />        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+          <link rel="stylesheet" href="./assets/css/style.css">
+      </head>
+      <body>
+          <header class='green lighten-1'>
+              <div class='green lighten-1'>
+                  <h1 class='center-align'>My Team</h1>
+              </div>
+          </header>
+          <main>
+              <section class='row'>
+                  ${generateHtml(employeeHtml)}
+              </section>
+          </main>
+      </body>
+      </html>
+      `;
+};
