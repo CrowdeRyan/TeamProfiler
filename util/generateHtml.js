38baed3f2b904df862a1df601f33f55794c0b38f
Ryan Crowder <crowderyan@gmail.com>
2021-07-07 20:29:43 -0400

    added engineer card creation

--- util/generateHtml.js
+++ util/generateHtml.js
@@ -30,4 +30,27 @@ const generateHtml = (employees) => {
                   </div>
               `;
   };
+  const createEngineer = (engineer) => {
+    return `
+                <div class="col s4">
+                    <div class="card green lighten-3">
+                        <div class="card content teal lighten-5 center-align">
+                            <h2 class="padding-top card-title">${engineer.getName()}</h2>
+                            <h5 class='padding-bot'><span class="icon icons"><i class="fas fa-laptop-code"></i></span>${engineer.getRole()}</h5>
+                        </div>
+                        <div class="card-content">
+                            <div class="card-content">
+                                <span>ID:${engineer.getId()}</span>
+                            </div>
+                            <div class="card-content">
+                                <span>Email:<a class='black-text' href='mailto:${engineer.getEmail()}'> ${engineer.getEmail()}</a></span>
+                            </div>
+                            <div class="card-content">
+                                <span>GitHub:<a href='https://github.com/${engineer.getGithub()}' target='_blank'> ${engineer.getGithub()}</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+  };
 };
